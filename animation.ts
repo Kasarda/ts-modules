3b402c6a8eaaf58d0aa7253d2629b98f05dbf311
Filip <kasardaafilip@gmail.com>
2017-10-15 18:23:36 +0200

    Updated modules and added @types packages

--- animation.ts
+++ animation.ts
@@ -1,14 +1,3 @@
-/**! @license
- *
- * @name Animation
- * 
- * http://animejs.com
- * @author Julian Garnier
- * @copyright ©2017 Julian Garnier
- * Released under the MIT license
- */
-
-
 /**
  * 
  * @module {animation}
@@ -25,43 +14,32 @@ import { error } from './common'
 
 
 
-/**! @license
- * 
- * @name bezier
- * https://github.com/gre/bezier-easing
- * BezierEasing - use bezier curve for transition easing function
- * by Gaëtan Renaudeau 2014 - 2015 – MIT License
- * 
- * Modify to TS by Filip Kasarda
- * 
- */
-
 /**
   *
-  * @function bezier
+  * {bezier}
   * Bezier timing function
   * bezier( a b c d ) (progress)
   *
 */
 
 
-const NEWTON_ITERATIONS: number = 4
-const NEWTON_MIN_SLOPE: number = 0.001
-const SUBDIVISION_PRECISION: number = 0.0000001
-const SUBDIVISION_MAX_ITERATIONS: number = 10
+const NEWTON_ITERATIONS = 4
+const NEWTON_MIN_SLOPE = 0.001
+const SUBDIVISION_PRECISION = 0.0000001
+const SUBDIVISION_MAX_ITERATIONS = 10
 
-let kSplineTableSize: number = 11
-let kSampleStepSize: number = 1.0 / (kSplineTableSize - 1.0)
+let kSplineTableSize = 11
+let kSampleStepSize = 1.0 / (kSplineTableSize - 1.0)
 
-let float32ArraySupported: boolean = typeof Float32Array === 'function'
+let float32ArraySupported = typeof Float32Array === 'function'
 
 const A: (aA1: number, aA2: number) => number = (aA1, aA2) => 1.0 - 3.0 * aA2 + 3.0 * aA1
 const B: (aA1: number, aA2: number) => number = (aA1, aA2) => 3.0 * aA2 - 6.0 * aA1
-const C: (aA1: number) => number =              (aA1)      => 3.0 * aA1
+const C: (aA1: number) => number = (aA1) => 3.0 * aA1
 const calcBezier: (aT: number, aA1: number, aA2: number) => number = (aT, aA1, aA2) => ((A(aA1, aA2) * aT + B(aA1, aA2)) * aT + C(aA1)) * aT
 const getSlope: (aT: number, aA1: number, aA2: number) => number = (aT, aA1, aA2) => 3.0 * A(aA1, aA2) * aT * aT + 2.0 * B(aA1, aA2) * aT + C(aA1)
 
-function binarySubdivide (aX: number, aA: number, aB: number, mX1: number, mX2: number): number {
+function binarySubdivide(aX: number, aA: number, aB: number, mX1: number, mX2: number): number {
   let currentX: number, currentT: number, i: number = 0
   do {
     currentT = aA + (aB - aA) / 2.0
@@ -75,21 +53,21 @@ function binarySubdivide (aX: number, aA: number, aB: number, mX1: number, mX2: 
   return currentT
 }
 
-function newtonRaphsonIterate (aX: number, aGuessT: number, mX1: number, mX2: number): number {
- for (let i = 0; i < NEWTON_ITERATIONS; ++i) {
-   let currentSlope: number = getSlope(aGuessT, mX1, mX2)
-   if (currentSlope === 0.0) {
-     return aGuessT
-   }
-   let currentX: number = calcBezier(aGuessT, mX1, mX2) - aX
-   aGuessT -= currentX / currentSlope
- }
- return aGuessT
+function newtonRaphsonIterate(aX: number, aGuessT: number, mX1: number, mX2: number): number {
+  for (let i = 0; i < NEWTON_ITERATIONS; ++i) {
+    let currentSlope: number = getSlope(aGuessT, mX1, mX2)
+    if (currentSlope === 0.0) {
+      return aGuessT
+    }
+    let currentX: number = calcBezier(aGuessT, mX1, mX2) - aX
+    aGuessT -= currentX / currentSlope
+  }
+  return aGuessT
 }
 
 
 
-export function bezier (mX1: number, mY1: number, mX2: number, mY2: number): any {
+export function bezier(mX1: number, mY1: number, mX2: number, mY2: number): any {
   if (!(0 <= mX1 && mX1 <= 1 && 0 <= mX2 && mX2 <= 1))
     error('bezier x values must be in [0, 1] range')
 
@@ -100,7 +78,7 @@ export function bezier (mX1: number, mY1: number, mX2: number, mY2: number): any
 
   }
 
-  function getTForX (aX: number): number {
+  function getTForX(aX: number): number {
     let intervalStart = 0.0
     let currentSample = 1
     let lastSample = kSplineTableSize - 1
@@ -123,7 +101,7 @@ export function bezier (mX1: number, mY1: number, mX2: number, mY2: number): any
 
   }
 
-  return function BezierEasing (x: number): number {
+  return function BezierEasing(x: number): number {
     if (mX1 === mY1 && mX2 === mY2)
       return x // linear
 
@@ -143,147 +121,153 @@ export function bezier (mX1: number, mY1: number, mX2: number, mY2: number): any
 
 /**
   *
-  * @function steps
+  * {steps}
   * Steps timing function
   * steps( steps ) (progress)
   *
 */
-export function steps(steps:number): (progress: number) => number {
-	return progress => Math.round(progress * steps) * (1 / steps)
+export function steps(steps: number): (progress: number) => number {
+  return progress => Math.round(progress * steps) * (1 / steps)
 }
 
 
 
 
-
-/**! @license
- * 
- * @name spring
- * Runge-Kutta spring physics function generator.
- * Adapted from Framer.js,
- * copyright Koen Bok.
- * MIT License: http://en.wikipedia.org/wiki/MIT_License
- * 
- * Modify to TS by Filip Kasarda
- * 
- */
-
-
-
 /**
   *
-  * @const spring
+  * {spring}
   * Spring physics timing function
   * spring(tension friction  duration ) (progress)
   *
 */
-export const spring = (function() {
-	const springAccelerationForState = (state:any) => (-state.tension * state.x) - (state.friction * state.v)
+export const spring = (function () {
+  const springAccelerationForState = (state: any) => (-state.tension * state.x) - (state.friction * state.v)
 
   function springEvaluateStateWithDerivative(initialState: any, dt: any, derivative: any): any {
-		const state: any = {
-			x: initialState.x + derivative.dx * dt,
-			v: initialState.v + derivative.dv * dt,
-			tension: initialState.tension,
-			friction: initialState.friction
-		}
+    const state: any = {
+      x: initialState.x + derivative.dx * dt,
+      v: initialState.v + derivative.dv * dt,
+      tension: initialState.tension,
+      friction: initialState.friction
+    }
 
-		return {dx: state.v, dv: springAccelerationForState(state)}
-	}
+    return { dx: state.v, dv: springAccelerationForState(state) }
+  }
 
-	function springIntegrateState(state: any, dt: any): any {
-		const a: any = {
-			dx: state.v,
-			dv: springAccelerationForState(state)
-		},
-				b = springEvaluateStateWithDerivative(state, dt * 0.5, a),
-				c = springEvaluateStateWithDerivative(state, dt * 0.5, b),
-				d = springEvaluateStateWithDerivative(state, dt, c),
-				dxdt = 1.0 / 6.0 * (a.dx + 2.0 * (b.dx + c.dx) + d.dx),
-				dvdt = 1.0 / 6.0 * (a.dv + 2.0 * (b.dv + c.dv) + d.dv)
+  function springIntegrateState(state: any, dt: any): any {
+    const a: any = {
+      dx: state.v,
+      dv: springAccelerationForState(state)
+    },
+      b = springEvaluateStateWithDerivative(state, dt * 0.5, a),
+      c = springEvaluateStateWithDerivative(state, dt * 0.5, b),
+      d = springEvaluateStateWithDerivative(state, dt, c),
+      dxdt = 1.0 / 6.0 * (a.dx + 2.0 * (b.dx + c.dx) + d.dx),
+      dvdt = 1.0 / 6.0 * (a.dv + 2.0 * (b.dv + c.dv) + d.dv)
 
-		state.x = state.x + dxdt * dt
-		state.v = state.v + dvdt * dt
+    state.x = state.x + dxdt * dt
+    state.v = state.v + dvdt * dt
 
-		return state
-	}
+    return state
+  }
 
-	return function springRK4Factory(tension: any, friction: any, duration?:any):any {
+  return function springRK4Factory(tension: any, friction: any, duration?: any): any {
 
-		let initState: any = {
-			x: -1,
-			v: 0,
-			tension: null,
-			friction: null
-		},
-			path = [0],
-			time_lapsed = 0,
-			tolerance = 1 / 10000,
-			DT = 16 / 1000,
-			have_duration, dt, last_state
+    let initState: any = {
+      x: -1,
+      v: 0,
+      tension: null,
+      friction: null
+    },
+      path = [0],
+      time_lapsed = 0,
+      tolerance = 1 / 10000,
+      DT = 16 / 1000,
+      have_duration, dt, last_state
 
-		tension = parseFloat(tension) || 500
-		friction = parseFloat(friction) || 20
-		duration = duration || null
+    tension = parseFloat(tension) || 500
+    friction = parseFloat(friction) || 20
+    duration = duration || null
 
-		initState.tension = tension
-		initState.friction = friction
+    initState.tension = tension
+    initState.friction = friction
 
-		have_duration = duration !== null
-		if (have_duration) {
-			time_lapsed = springRK4Factory(tension, friction)
-			dt = time_lapsed / duration * DT
-		} else {
-			dt = DT
-		}
+    have_duration = duration !== null
+    if (have_duration) {
+      time_lapsed = springRK4Factory(tension, friction)
+      dt = time_lapsed / duration * DT
+    } else {
+      dt = DT
+    }
 
-		while (true) {
-			last_state = springIntegrateState(last_state || initState, dt)
-			path.push(1 + last_state.x)
-			time_lapsed += 16
-			if (!(Math.abs(last_state.x) > tolerance && Math.abs(last_state.v) > tolerance)) {
-				break
-			}
-		}
-		return !have_duration ? time_lapsed : function(percentComplete:number):number {
-      return path[ (percentComplete * (path.length - 1)) | 0 ]
-		}
-	}
+    while (true) {
+      last_state = springIntegrateState(last_state || initState, dt)
+      path.push(1 + last_state.x)
+      time_lapsed += 16
+      if (!(Math.abs(last_state.x) > tolerance && Math.abs(last_state.v) > tolerance)) {
+        break
+      }
+    }
+    return !have_duration ? time_lapsed : function (percentComplete: number): number {
+      return path[(percentComplete * (path.length - 1)) | 0]
+    }
+  }
 }())
 
 
 
-/**
- * 
- * @var easings
- * Cubic bezier shorthands
- * 
- */
+export interface Easings {
+  ease(progress: number): number
+  easeIn(progress: number): number
+  easeOut(progress: number): number
+  easeInOut(progress: number): number
+  easeInSine(progress: number): number
+  easeOutSine(progress: number): number
+  easeInOutSine(progress: number): number
+  easeInQuad(progress: number): number
+  easeOutQuad(progress: number): number
+  easeInOutQuad(progress: number): number
+  easeInCubic(progress: number): number
+  easeOutCubic(progress: number): number
+  easeInOutCubic(progress: number): number
+  easeInQuart(progress: number): number
+  easeOutQuart(progress: number): number
+  easeInOutQuart(progress: number): number
+  easeInQuint(progress: number): number
+  easeOutQuint(progress: number): number
+  easeInOutQuint(progress: number): number
+  easeInExpo(progress: number): number
+  easeOutExpo(progress: number): number
+  easeInOutExpo(progress: number): number
+  easeInCirc(progress: number): number
+  easeOutCirc(progress: number): number
+  easeInOutCirc(progress: number): number
+}
 
-export let easings: any = {
-    ease: bezier(0.25, 0.1, 0.25, 1.0),
-    easeIn: bezier(0.42, 0.0, 1.00, 1.0),
-    easeOut: bezier(0.00, 0.0, 0.58, 1.0),
-    easeInOut: bezier(0.42, 0.0, 0.58, 1.0),
-    easeInSine: bezier(0.47, 0, 0.745, 0.715),
-    easeOutSine: bezier(0.39, 0.575, 0.565, 1),
-    easeInOutSine: bezier(0.445, 0.05, 0.55, 0.95),
-    easeInQuad: bezier(0.55, 0.085, 0.68, 0.53),
-    easeOutQuad: bezier(0.25, 0.46, 0.45, 0.94),
-    easeInOutQuad: bezier(0.455, 0.03, 0.515, 0.955),
-    easeInCubic: bezier(0.55, 0.055, 0.675, 0.19),
-    easeOutCubic: bezier(0.215, 0.61, 0.355, 1),
-    easeInOutCubic: bezier(0.645, 0.045, 0.355, 1),
-    easeInQuart: bezier(0.895, 0.03, 0.685, 0.22),
-    easeOutQuart: bezier(0.165, 0.84, 0.44, 1),
-    easeInOutQuart: bezier(0.77, 0, 0.175, 1),
-    easeInQuint: bezier(0.755, 0.05, 0.855, 0.06),
-    easeOutQuint: bezier(0.23, 1, 0.32, 1),
-    easeInOutQuint: bezier(0.86, 0, 0.07, 1),
-    easeInExpo: bezier(0.95, 0.05, 0.795, 0.035),
-    easeOutExpo: bezier(0.19, 1, 0.22, 1),
-    easeInOutExpo: bezier(1, 0, 0, 1),
-    easeInCirc: bezier(0.6, 0.04, 0.98, 0.335),
-    easeOutCirc: bezier(0.075, 0.82, 0.165, 1),
-    easeInOutCirc: bezier(0.785, 0.135, 0.15, 0.86)
+export let easings: Easings = {
+  ease: bezier(0.25, 0.1, 0.25, 1.0),
+  easeIn: bezier(0.42, 0.0, 1.00, 1.0),
+  easeOut: bezier(0.00, 0.0, 0.58, 1.0),
+  easeInOut: bezier(0.42, 0.0, 0.58, 1.0),
+  easeInSine: bezier(0.47, 0, 0.745, 0.715),
+  easeOutSine: bezier(0.39, 0.575, 0.565, 1),
+  easeInOutSine: bezier(0.445, 0.05, 0.55, 0.95),
+  easeInQuad: bezier(0.55, 0.085, 0.68, 0.53),
+  easeOutQuad: bezier(0.25, 0.46, 0.45, 0.94),
+  easeInOutQuad: bezier(0.455, 0.03, 0.515, 0.955),
+  easeInCubic: bezier(0.55, 0.055, 0.675, 0.19),
+  easeOutCubic: bezier(0.215, 0.61, 0.355, 1),
+  easeInOutCubic: bezier(0.645, 0.045, 0.355, 1),
+  easeInQuart: bezier(0.895, 0.03, 0.685, 0.22),
+  easeOutQuart: bezier(0.165, 0.84, 0.44, 1),
+  easeInOutQuart: bezier(0.77, 0, 0.175, 1),
+  easeInQuint: bezier(0.755, 0.05, 0.855, 0.06),
+  easeOutQuint: bezier(0.23, 1, 0.32, 1),
+  easeInOutQuint: bezier(0.86, 0, 0.07, 1),
+  easeInExpo: bezier(0.95, 0.05, 0.795, 0.035),
+  easeOutExpo: bezier(0.19, 1, 0.22, 1),
+  easeInOutExpo: bezier(1, 0, 0, 1),
+  easeInCirc: bezier(0.6, 0.04, 0.98, 0.335),
+  easeOutCirc: bezier(0.075, 0.82, 0.165, 1),
+  easeInOutCirc: bezier(0.785, 0.135, 0.15, 0.86)
 }
--- parallax.ts
+++ parallax.ts
@@ -1,14 +1,3 @@
-/**! @license
-  * parallax.ts
-  *
-  * This source code is licensed under the GNU GENERAL PUBLIC LICENSE found in the
-  * LICENSE file in the root directory of this source tree.
-  *
-  * Copyright (c) 2017-Present, Filip Kasarda
-  *
-  */
-
-
 /**
   *
   * @module parallax
@@ -20,41 +9,63 @@
 import { getValue, getProgress, inView, createRequestFrame, setStyles } from './common'
 
 
-interface Properties {
-    translateX?: number[]
-    translateY?: number[]
-    translateZ?: number[]
-    translate?: number[]
-
-    scale?: number[]
-    scaleX?: number[]
-    scaleY?: number[]
-    scaleZ?: number[]
-
-    rotate?: number[]
-    rotateX?: number[]
-    rotateY?: number[]
-    rotateZ?: number[]
-
-    skew?: number[]
-    skewX?: number[]
-    skewY?: number[]
-
-    perspective?: number[]
-
-    opacity?: number[]
-}
 
 
 export interface ParallaxOptions extends Properties {
-    targets: any
-    duration?: number
-    timing?: (index?: number, animation_prog?: number, scroll_prog?: number, event?: any) => number,
-    progress?: (index?: number, scroll_prog?:number, event?: any) => void
-    deepProgress?: (index?: number, scroll_prog?: number, animation_prog?: number, event?:any) => void
-  }
+  targets: any
+  duration?: number
+
+  progress?: (this: any, data: DataArgument) => void
+  timing?: (this: any, data: DataArgument & TimingDataArgument) => number
+  deepProgress?: (this: any, data: DataArgument & DeepProgressDataArgument) => void
+}
 
 
+interface Properties {
+  translateX?: number[]
+  translateY?: number[]
+  translateZ?: number[]
+  translate?: number[]
+
+  scale?: number[]
+  scaleX?: number[]
+  scaleY?: number[]
+  scaleZ?: number[]
+
+  rotate?: number[]
+  rotateX?: number[]
+  rotateY?: number[]
+  rotateZ?: number[]
+
+  skew?: number[]
+  skewX?: number[]
+  skewY?: number[]
+
+  perspective?: number[]
+
+  opacity?: number[]
+}
+
+interface DataArgument {
+  element: any
+  index: number
+
+  scrollProgress: number
+  progress?: number
+  timingProgress?: number
+
+  event: ParallaxEvent
+}
+
+interface TimingDataArgument { progress: number }
+interface DeepProgressDataArgument extends TimingDataArgument { timingProgress: number }
+
+interface ParallaxEvent extends Event {
+  scrollTop: number
+  element: any
+  inView: boolean
+  options: ParallaxOptions
+}
 
 
 
@@ -67,148 +78,157 @@ export interface ParallaxOptions extends Properties {
 
 export function Parallax(options: any) {
 
+  /**
+    *
+    * Settings constant from {options} param
+    *
+  */
+  const targets: any = options.targets
+  const elements: Element[] = typeof targets === 'string' ? Array.from(document.querySelectorAll(targets)) : targets.length ? Array.from(targets) : [targets]
+  const duration: number = options.duration || 400
+  const timing: (this: Element, data: DataArgument & TimingDataArgument) => number = options.timing
+
+  const list_of_supported_properties: (keyof Properties)[] = ['translateX', 'translateY', 'scale', 'rotate', 'opacity', 'skewX', 'skewY', 'skew', 'translateZ', 'rotateZ', 'rotateX', 'rotateY', 'scaleX', 'scaleY', 'scaleZ', 'translate', 'perspective']
+  const properties: any = {}
+  list_of_supported_properties.forEach(prop => options[prop] && (properties[prop] = options[prop]))
+
+
+  elements.forEach((elem: any, index: number): void => {
+
+
     /**
       *
-      * Settings constant from {options} param
+      * Set scroll position before update to define min and max value of css property
       *
     */
-    const targets: any = options.targets
-    const elements: Element[] = typeof targets === 'string' ? Array.from(document.querySelectorAll(targets)) : targets.length ? Array.from(targets) : [targets]
-    const duration: number = options.duration || 400
-    const timing: (progress: number) => number = options.timing
-
-    const list_of_supported_properties: string[] = ['translateX', 'translateY', 'scale', 'rotate', 'opacity', 'skewX', 'skewY', 'skew', 'translateZ', 'rotateZ', 'rotateX', 'rotateY', 'scaleX', 'scaleY', 'scaleZ', 'translate', 'perspective']
-    const properties: any = {}
-    list_of_supported_properties.forEach(prop => options[prop] && (properties[prop] = options[prop]))
+    let scroll_position_before: number = document.documentElement.scrollTop
 
 
-    elements.forEach((elem: any, index: number): void => {
+    const elem_rect: ClientRect = elem.getBoundingClientRect()
+    const min: number = ((elem_rect.top) + document.documentElement.scrollTop) - window.innerHeight
+    const max: number = ((elem_rect.top) + document.documentElement.scrollTop) + elem_rect.height
+
+    /**
+      *
+      * @function update
+      * Update css property and create animation
+    */
+
+    function update(event: ParallaxEvent): void {
+      const scroll_position: number = document.documentElement.scrollTop
+
+      event.scrollTop = scroll_position
+      event.element = elem
+      event.inView = inView(elem)
+      event.options = options
+
+      /**
+        *
+        * Get default value and value of transform function or opacity
+        *
+      */
+      const transform: any[] = []
+      const opacity: number[] = []
+      let scroll_progress: any
+      for (const prop in properties) {
+
+        const prop_val: number[] = properties[prop]
+
+        /**
+          *
+          * Get default value and value of transform function or opacity
+          *
+        */
+        const minus_max: number = prop_val[2] || 0
+        const minus_min: number = prop_val[3] || 0
+
+        scroll_progress = getProgress(min - minus_min, max - minus_max, scroll_position)
+        const scroll_def_progress: number = getProgress(min - minus_min, max - minus_max, scroll_position_before)
+
+        const value: number = getValue(prop_val[0], prop_val[1], scroll_progress)
+        const default_value: number = getValue(prop_val[0], prop_val[1], scroll_def_progress)
+
+        /**
+          *
+          * Get unit of transform function
+          *
+        */
+        let unit: string = 'px'
+
+        if (prop.match(/^(rotate|rotateX|rotateZ|rotateY|skew|skewX|skewY)$/)) unit = 'deg'
+        if (prop.match(/^(scale|scaleX|scaleY|scaleZ)$/)) unit = ''
 
 
         /**
           *
-          * Set scroll position before update to define min and max value of css property
+          * Push data for @function render function
           *
         */
-        let scroll_position_before: number = document.documentElement.scrollTop
+        if (prop === 'opacity')
+          opacity.push(default_value, value)
+        else
+          transform.push([prop, default_value, value, unit])
 
+      }
 
-        const elem_rect: ClientRect = elem.getBoundingClientRect()
-        const min: number = ((elem_rect.top) + document.documentElement.scrollTop) - window.innerHeight
-        const max: number = ((elem_rect.top) + document.documentElement.scrollTop) + elem_rect.height
+      const data_argument: DataArgument = {
+        element: elem,
+        index,
+        scrollProgress: scroll_progress,
+        event
+      }
 
+      if (typeof options.progress === 'function')
+        options.progress.call(elem, data_argument)
 
+      /**
+        *
+        * @function render
+        * Creating animation
+        *
+      */
+      function render(p: number): void {
 
-        /**
-          *
-          * @function update
-          * Update css property and create animation
-        */
+        data_argument.progress = p
+        const progress: number = timing ? timing.call(elem, data_argument) : p
+        data_argument.timingProgress = progress
 
-        function update(event: any): void {
-          const scroll_position: number = document.documentElement.scrollTop
-          
-          event.scrollTop = scroll_position
-          event.element = elem
-          event.inView = inView(elem)
-          event.options = options
-
-            /**
-              *
-              * Get default value and value of transform function or opacity
-              *
-            */
-            const transform: any[] = []
-            const opacity: number[] = []
-            let scroll_progress: any
-            for (const prop in properties) {
-
-                const prop_val: number[] = properties[prop]
-
-                /**
-                  *
-                  * Get default value and value of transform function or opacity
-                  *
-                */
-                const minus_max: number = prop_val[2] || 0
-                const minus_min: number = prop_val[3] || 0
-
-                scroll_progress = getProgress(min - minus_min, max - minus_max, scroll_position)
-                const scroll_def_progress: number = getProgress(min - minus_min, max - minus_max, scroll_position_before)
-
-                const value: number = getValue(prop_val[0], prop_val[1], scroll_progress)
-                const default_value: number = getValue(prop_val[0], prop_val[1], scroll_def_progress)
-
-                /**
-                  *
-                  * Get unit of transform function
-                  *
-                */
-                let unit: string = 'px'
-
-                if (prop.match(/^(rotate|rotateX|rotateZ|rotateY|skew|skewX|skewY)$/)) unit = 'deg'
-                if (prop.match(/^(scale|scaleX|scaleY|scaleZ)$/)) unit = ''
-
-
-                /**
-                  *
-                  * Push data for @function render function
-                  *
-                */
-                if (prop === 'opacity')
-                    opacity.push(default_value, value)
-                else
-                    transform.push([prop, default_value, value, unit])
-
-            }
-
-            if (typeof options.progress === 'function')
-              options.progress.call(elem, index, scroll_progress, event)
-
-            /**
-              *
-              * @function render
-              * Creating animation
-              *
-            */
-            function render(p: number): void {
-              const progress: number = timing ? timing.call(elem, index, p, scroll_progress, event) : p
-
-                if (transform.length) {
-                    let transform_string: string = ''
-                    transform.forEach(trans => {
-                        const transform_value: number = getValue(trans[1], trans[2], progress)
-                        transform_string += trans[0] + '(' + transform_value + trans[3] + ') '
-                    })
-                    setStyles(elem, 'transform', transform_string)
-                }
-
-                if (opacity.length)
-                    elem.style.opacity = getValue(opacity[0], opacity[1], progress)
-               
-               if (typeof options.deepProgress === 'function')
-                 options.deepProgress.call(elem, index, scroll_progress, progress, event)
-              }
-
-
-            createRequestFrame(duration, render)
-
-
-            /* updating scroll_position_before */
-            scroll_position_before = scroll_position
+        if (transform.length) {
+          let transform_string: string = ''
+          transform.forEach(trans => {
+            const transform_value: number = getValue(trans[1], trans[2], progress)
+            transform_string += trans[0] + '(' + transform_value + trans[3] + ') '
+          })
+          setStyles(elem, 'transform', transform_string)
         }
 
+        if (opacity.length)
+          elem.style.opacity = getValue(opacity[0], opacity[1], progress)
+
+
+        if (typeof options.deepProgress === 'function')
+          options.deepProgress.call(elem, { element: elem, index, scrollProgress: scroll_progress, timingProgress: progress, progress: p, event })
+      }
+
+
+      createRequestFrame(duration, render)
+
+
+      /* updating scroll_position_before */
+      scroll_position_before = scroll_position
+    }
 
 
 
 
-        /**
-          *
-          * Initialize
-          *
-        */
-        update({})
-        document.addEventListener('scroll', update)
 
-    })
+    /**
+      *
+      * Initialize
+      *
+    */
+    update(<any>{})
+    document.addEventListener('scroll', update)
+
+  })
 }
