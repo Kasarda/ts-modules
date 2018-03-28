8ffc0e79b2c6809c34cc0a4c8612877fdfefa63e
Filip <kasardaafilip@gmail.com>
2018-03-28 20:12:08 +0200

    Update

--- worker.js
+++ worker.js
@@ -1,5 +1,4 @@
 /**! @license
-  * worker.ts
   *
   * This source code is licensed under the GNU GENERAL PUBLIC LICENSE found in the
   * LICENSE file in the root directory of this source tree.
@@ -7,44 +6,43 @@
   * Copyright (c) 2017-Present, Filip Kasarda
   *
   */
-define(["require", "exports"], function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    var WebWorker = /** @class */ (function () {
-        function WebWorker(worker) {
-            if (worker === void 0) { worker = self; }
-            this.terminated = false;
-            this.sended = [];
-            this.readed = [];
-            this.worker = worker === self ? worker : new worker;
-        }
-        WebWorker.prototype.send = function (name, data) {
-            this.worker.postMessage({ name: name, data: data });
-            this.sended.push(name);
-            return this;
-        };
-        WebWorker.prototype.read = function (name, callback) {
-            var scope = this;
-            this.worker.addEventListener('message', function (event) {
-                if (name === event.data.name) {
-                    callback.call(this, event.data.data, event);
-                    scope.readed.push(name);
-                }
-            });
-            return this;
-        };
-        WebWorker.prototype.failed = function (listener) {
-            this.worker.addEventListener('error', listener);
-            return this;
-        };
-        WebWorker.prototype.terminate = function () {
-            if (this.worker.terminate) {
-                this.worker.terminate();
-                this.terminated = true;
+
+
+export default class WebWorker {
+    constructor(worker = self) {
+        this.worker = worker === self ? worker : new worker()
+        this.terminated = false
+        this.sended = []
+        this.readed = []
+    }
+
+    send(name, data) {
+        this.worker.postMessage({ name, data })
+        this.sended.push(name)
+        return this
+    }
+
+    read(name, callback) {
+        const scope = this
+        this.worker.addEventListener('message', function (event) {
+            if (name === event.data.name) {
+                callback.call(this, event.data.data, event)
+                scope.readed.push(name)
             }
-            return this;
-        };
-        return WebWorker;
-    }());
-    exports.WebWorker = WebWorker;
-});
+        })
+        return this
+    }
+
+    failed(listener) {
+        this.worker.addEventListener('error', listener)
+        return this
+    }
+
+    terminate() {
+        if (this.worker.terminate) {
+            this.worker.terminate()
+            this.terminated = true
+        }
+        return this
+    }
+}
